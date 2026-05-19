cb5eaff30007f4f848b25bd404b9d5ac202ce393
Theridel <simone.python3@gmail.com>
2026-05-19 20:44:21 +0200

    Change directory command to user creation script

--- start.sh
+++ start.sh
@@ -14,4 +14,4 @@ echo "Hello World! Sto preparando l'ambiente per i miei agenti."
 # Mostra chi sta eseguendo lo script (per capire se sei root o user)
 echo "Attualmente sto lavorando come l'utente: $(whoami)"
 
-cd /home
+crea_utente.sh
